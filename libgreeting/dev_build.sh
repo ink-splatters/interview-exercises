c70bb41e0c2b77b054df018a9eb300fb4a902aa3
Peter Asanov <ink.splatters@pm.me>
2021-09-21 11:27:42 +0200

    dev build script

---
+++ libgreeting/dev_build.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/env bash
+
+run() {
+  echo -e "\e[36m ➤ $*\e[0m"
+  $@
+}
+
+if ! command -v poetry 2>&1 > /dev/null ; then
+  echo -e "\e[33mERROR: Python poetry is required. Install using:\e[0m\n"
+
+  echo -e " \e[36m1. pipx[recommended]:\e[32m"
+cat <<- EOF
+
+    pip install -U pip wheel pipx # --user if needd
+    pipx ensurepath
+    source ~/.bashrc  # change to your shell's config name
+    if command -v poetry &2>1 > /dev/null ; then pipx upgrade poetry ; else pipx install poetry ; fi
+    pipx install poetry
+EOF
+echo -e "\n \e[36m2. pip:\e[32m\n"
+cat <<- EOF
+    pip install -U pip wheel poetry # --user if needed
+    if command -v poetry &2>1 > /dev/null ; then pipx upgrade poetry ; else pipx install poetry ; fi
+EOF
+
+
+#   install poetry # preferred, or"
+  echo "    pip install -U pip wheel poetry"
+  exit 1
+fi
+
+current_dir=
+echo $current_dir
+run poetry install
+run poetry run conan source $(dirname $0)
+
+#conan source .
+#conan install . --install-folder=./build
+#conan build . --build-folder=./build

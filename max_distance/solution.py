2fd65ecdcceccff0a0adc191c271e5af096d0398
Peter A <ink.splatters@pm.me>
2021-09-27 10:14:38 +0200

    solve

--- max_distance/solution.py
+++ max_distance/solution.py
@@ -1,23 +1,47 @@
-import multimethod as MM
+# import multimethod as MM
+#
+# from .perf_hint import *
+#
+#
+# @MM.multimethod
+# def solve(perf_hint: PerfHint):
+#     raise NotImplementedError("not implemented")
+#
+#
+# @solve.register(Square)
+# def solve_NxN():
+#     print('Square')
+#
+#
+# @solve.register(Log)
+# def solve_logarithmic():
+#     print('Log, that is better')
+#
+#
+# # @solve.register(Linear)
 
-from .perf_hint import *
+
+from typing import List, Dict
 
 
-@MM.multimethod
-def solve(perf_hint: PerfHint):
-    raise NotImplementedError("not implemented")
+def solve(A: List[int]) -> int:
+    d: Dict[int, int] = {}  # element -> index
+
+    max_distance = 0
+    for i, x in enumerate(A):
+        if not x in d:
+            d[x] = i
+        else:
+            max_distance = max(max_distance, abs(i - d[x]))
+
+    return max_distance
 
 
-@solve.register(Square)
-def solve_NxN():
-    print('Square')
+print(solve([1, 2, 3, 5, 1, 2]))
+print(solve([1, 1, 1, 1, 1, 1, 1]))
+print(solve([]))
+print(solve([1, 2, 3]))
+print(solve([1, 2, 3, 4]))
 
-
-@solve.register(Log)
-def solve_logarithmic():
-    print('Log, that is better')
-
-
-@solve.register(Linear)
-def solve_linear():
-    print('Wow')
+# for x, y in enumerate(A[0:mid]), enumerate(reversed(A[mid:])):
+#     print((x, y))
